6516f34837d1964af5cd518d9c39c1d0f1ee3dba
Nitish <37649484+mehtanitish875@users.noreply.github.com>
2022-05-16 22:20:26 +0530

    code update

---
+++ android/proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class com.cleardil.reactnative.sdk.** { *; }
---
+++ index.js
@@ -0,0 +1,6 @@
+import ClearDil from "./js/ClearDil";
+export { ClearDilDocumentType, ClearDilCaptureType } from './js/config_constants';
+
+export { ClearDil };
+
+export default ClearDil;
